ba5919ba1a5fc708bbc43aca419cdf3c7ca661df
kalpataru123 <sahookalpataru683@gmail.com>
2025-11-30 11:58:42 +0530

    first commit

---
+++ vite_react_01/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+import Hello from "./Hello"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>learning react series</h1>
+      <Hello/>
+      
+    </>
+  )
+}
+
+export default App
